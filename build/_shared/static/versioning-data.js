e4b97be2e0de5fe989217c7bc231082a79093453
noahcostantini20 <noah.costantini@arsautomation.com>
2026-05-29 17:39:56 +0200

    Added Rotary Disks

--- build/_shared/static/versioning-data.js
+++ build/_shared/static/versioning-data.js
@@ -5,17 +5,10 @@ window.FV_VERSIONING = {
   "languagesByVersion": {
     "V. 1.0": [
       "EN",
-<<<<<<< Updated upstream
-      "IT"
-=======
       "IT",
       "FR",
       "DE",
       "ES"
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> dfe7f519566c8dfae15f4b3484503a4a6f4f0ab9
     ]
   },
   "defaultLanguageByVersion": {
