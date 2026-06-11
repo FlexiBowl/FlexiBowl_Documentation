31a82c085ba88e62b38068b45f69c873c6c2435e
noahcostantini20 <noah.costantini@arsautomation.com>
2026-06-11 08:49:40 +0200

    Last Rotary Disks Images 2

---
+++ build/_shared/static/versioning-data.js
@@ -0,0 +1,17 @@
+window.FV_VERSIONING = {
+  "versions": [
+    "V. 1.0"
+  ],
+  "languagesByVersion": {
+    "V. 1.0": [
+      "EN",
+      "IT",
+      "FR",
+      "DE",
+      "ES"
+    ]
+  },
+  "defaultLanguageByVersion": {
+    "V. 1.0": "IT"
+  }
+};
